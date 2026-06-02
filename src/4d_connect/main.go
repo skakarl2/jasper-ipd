1bbd7ce7a4cad65885c87b6e8572166497822fb6
Jay Ricco <ricco@synopsys.com>
2026-06-02 16:26:59 -0400

    p2

--- src/4d_connect/main.go
+++ src/4d_connect/main.go
@@ -7,8 +7,10 @@ func main() {
 	fmt.Println("--------------")
 	fmt.Println("Two players alternate dropping pieces into a 4x4 board of columns.")
 	fmt.Println("Columns are addressed by x y where x,y are 1..4.")
+	fmt.Println("--------------")
 	fmt.Println("A piece falls to the lowest free z in that column.")
 	fmt.Println("First player to align 4 pieces in any 3D straight line wins.")
+	fmt.Println("--------------")
 	fmt.Println()
 
 	game := NewGame()
