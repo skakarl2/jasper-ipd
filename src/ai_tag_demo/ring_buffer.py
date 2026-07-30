e0aaedf40305e33ddc6c5c222c4165de1f41424e
ricco <ricco@blackduck.com>
2026-07-30 18:40:34 -0400

    iter10 HUMAN_CREATE ring_buffer hand-authored

---
+++ src/ai_tag_demo/ring_buffer.py
@@ -0,0 +1,50 @@
+"""Small hand-written ring buffer (human origin).
+
+A fixed-capacity circular buffer with overwrite-on-full semantics. Written
+by hand to serve as a human-origin baseline in the attribution matrix test.
+"""
+
+
+class RingBuffer:
+    def __init__(self, capacity):
+        if capacity <= 0:
+            raise ValueError("capacity must be positive")
+        self._cap = capacity
+        self._buf = [None] * capacity
+        self._head = 0
+        self._count = 0
+
+    def __len__(self):
+        return self._count
+
+    def is_full(self):
+        return self._count == self._cap
+
+    def push(self, item):
+        tail = (self._head + self._count) % self._cap
+        if self.is_full():
+            # overwrite the oldest slot and advance head
+            self._buf[self._head] = item
+            self._head = (self._head + 1) % self._cap
+        else:
+            self._buf[tail] = item
+            self._count += 1
+
+    def pop(self):
+        if self._count == 0:
+            raise IndexError("pop from empty ring buffer")
+        item = self._buf[self._head]
+        self._buf[self._head] = None
+        self._head = (self._head + 1) % self._cap
+        self._count -= 1
+        return item
+
+    def to_list(self):
+        return [self._buf[(self._head + i) % self._cap] for i in range(self._count)]
+
+
+if __name__ == "__main__":
+    rb = RingBuffer(3)
+    for x in [1, 2, 3, 4]:
+        rb.push(x)
+    print(rb.to_list())
